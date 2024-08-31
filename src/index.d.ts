75643d73d16023f7f83751fe50b655d33fb89c99
Dmitry Baranovskiy <baranovs@adobe.com>
2024-08-31 22:24:15 +1000

    Changed search to reverse function
    Replaced search with algebra.

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,5 +4,5 @@ export = bezier;
 declare function bezier(x1: number, y1: number, x2: number, y2: number): bezier.EasingFunction;
 
 declare namespace bezier {
-    export interface EasingFunction { (input: number): number }
+    export type EasingFunction = (input: number) => number
 }
